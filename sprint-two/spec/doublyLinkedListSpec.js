def560236707d4b7278671b3d85e09aa58dd8817
Delphine Foo-Matkin <delphinef@gmail.com>
2015-11-25 12:02:11 -0800

    Added Doubly Linked List test for 'addToTail' and passed it.

--- sprint-two/spec/doublyLinkedListSpec.js
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -18,40 +18,40 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList.contains).to.be.a("function");
   });
 
- /* it('should designate a new tail when new nodes are added', function(){
-    linkedList.addToTail(4);
-    expect(linkedList.tail.value).to.equal(4);
-    linkedList.addToTail(5);
-    expect(linkedList.tail.value).to.equal(5);
+  it('should designate a new tail when new nodes are added', function(){
+    doublyLinkedList.addToTail(4);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
   });
 
-  it('should remove the head from the list when removeHead is called', function(){
+/*  it('should remove the head from the list when removeHead is called', function(){
     linkedList.addToTail(4);
     linkedList.addToTail(5);
     expect(linkedList.head.value).to.equal(4);
     linkedList.removeHead();
     expect(linkedList.head.value).to.equal(5);
-  });
+  });*/
 
-  it("should return the value of the former head when removeHead is called", function(){
+  /*it("should return the value of the former head when removeHead is called", function(){
     linkedList.addToTail(4);
     expect(linkedList.removeHead()).to.equal(4);
   });
-
-  it("should contain a value that was added", function(){
+*/
+  /*it("should contain a value that was added", function(){
     linkedList.addToTail(4);
     linkedList.addToTail(5);
     expect(linkedList.contains(4)).to.equal(true);
     expect(linkedList.contains(5)).to.equal(true);
     expect(linkedList.contains(6)).to.equal(false);
   });
-
-  it('should not contain a value that was removed', function(){
+*/
+ /* it('should not contain a value that was removed', function(){
     linkedList.addToTail(4);
     linkedList.addToTail(5);
     linkedList.removeHead();
     expect(linkedList.contains(4)).to.equal(false);
-  });
-*/
+  });*/
+
   // add more tests here to test the functionality of linkedList
 });
--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -1,17 +1,30 @@
 var DoublyLinkedList = function() {
   var list = {};
-  var prevNode = {};
-  var nextNode = {};
 
   list.head = null;
   list.tail = null;
 
   list.addToHead = function(value) {
-
+    var newNode = Node(value);
   }
 
   list.addToTail = function(value) {
-
+    var newNode = Node(value);
+    newNode.next = null;
+    //if there are no values in the list
+    if (list.head === null) {
+      //set head and tail to value
+      list.head = newNode;
+      list.tail = newNode;
+    //else
+    } else {
+      //set last node to point to value
+      list.tail.next = newNode;
+      //set new node to point to previous node
+      newNode.previous = list.tail;
+      //and set tail to point to value
+      list.tail = newNode;
+    }
   }
 
   list.removeHead = function(value) {
