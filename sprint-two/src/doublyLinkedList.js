90b46231be4c9f61e977431b10176c175e724624
Delphine Foo-Matkin <delphinef@gmail.com>
2015-11-25 11:39:44 -0800

    Added test to Doubly Linked list testing for existence of head and tail, and passed.

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,18 @@
+var DoublyLinkedList = function() {
+  var list = {};
+  var prevNode = {};
+  var nextNode = {};
+
+  list.head = null;
+  list.tail = null;
+
+  list.addToHead = function(value) {
+
+  }
+
+  list.removeTail = function(value) {
+
+  }
+
+  return list;
+}
